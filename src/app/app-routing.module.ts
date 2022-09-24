10696a0d58c7b55773c33b7633e230597deb6c03
unknown <ajinkya.chanshetty@gmail.com>
2022-09-24 23:35:32 +0100

    Updated the carousel design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './shared/login/login.component';
+import { LogoutComponent } from './shared/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', component: LoginComponent },
+{ path: 'logout', component: LogoutComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
