8db812a05698420d11d1e2b76cea2db0fc437505
unknown <ajinkya.chanshetty@gmail.com>
2022-09-25 23:26:04 +0100

    Updated the routings and carousel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './shared/login/login.component';
 import { LogoutComponent } from './shared/logout/logout.component';
+import { SharedModule } from './shared/shared.module';
 
-const routes: Routes = [{ path: 'login', component: LoginComponent },
-{ path: 'logout', component: LogoutComponent }];
+const routes: Routes = [{ path: '', loadChildren:()=> SharedModule}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/shared-routing.module.ts
+++ src/app/shared/shared-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'login', component: LoginComponent },
+{ path: 'logout', component: LogoutComponent },
+{path:'home', component:CarouselComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
