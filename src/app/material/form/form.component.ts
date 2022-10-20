4acf1f0142815f8a44bfffc959fae6ca767bc971
unknown <ajinkya.chanshetty@gmail.com>
2022-10-20 23:58:43 +0100

    updated the datepicker

--- src/app/material/form/form.component.ts
+++ src/app/material/form/form.component.ts
@@ -12,5 +12,10 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  myFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6;
+  };
 
 }
