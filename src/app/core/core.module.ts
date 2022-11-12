5c26954f50bf0369b0c5467f490bd2cfa1060fa2
unknown <ajinkya.chanshetty@gmail.com>
2022-11-12 23:47:28 +0000

    Updated custom Date directive

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,15 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { DashboardDirective } from './directives/dashboard.directive';
+import { CustomDateDirective } from './directives/custom-date.directive';
 
 
 @NgModule({
   declarations: [
-    DashboardDirective
+    DashboardDirective,
+    CustomDateDirective
   ],
   imports: [
     CommonModule,
     CoreRoutingModule
+  ],
+  exports: [
+    CustomDateDirective
   ]
 })
 export class CoreModule { }
